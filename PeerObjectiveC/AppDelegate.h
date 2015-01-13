d72f603f7c7fcae0f0a31cd26cbd68acace74fd5
Shin Hiroe <hiroe.orz@gmail.com>
2015-01-13 23:49:22 +0900

    Initial Commit

---
+++ PeerObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeerObjectiveC
+//
+//  Created by HIROE Shin on 2015/01/13.
+//  Copyright (c) 2015年 Shin Hiroe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PeerObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeerObjectiveC
+//
+//  Created by HIROE Shin on 2015/01/13.
+//  Copyright (c) 2015年 Shin Hiroe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
